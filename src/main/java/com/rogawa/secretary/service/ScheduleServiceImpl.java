b75925f0984fed97d79f1e054011062f39dc7322
rogawa14106 <ookawa14106@gmail.com>
2025-07-15 23:28:00 +0900

    fix: ハッシュ生成箇所を修正。生成されたカラーコードが暗すぎたので修正

--- src/main/java/com/rogawa/secretary/service/ScheduleServiceImpl.java
+++ src/main/java/com/rogawa/secretary/service/ScheduleServiceImpl.java
@@ -4,6 +4,8 @@ import com.rogawa.secretary.model.Schedule;
 import com.rogawa.secretary.repository.ScheduleRepository;
 
 import java.awt.Color;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
@@ -110,15 +112,26 @@ public class ScheduleServiceImpl implements ScheduleService {
     }
 
     public String generateOwnerColorCode(String ownerTxt) {
-        // 所有者名からハッシュ値を取得
-        int hash = ownerTxt.hashCode();
+        int hash = 0;
+        try {
+            // 予定の所有者文字列からSHA-256でハッシュ値を生成
+            // Warning: 環境によってSHA-256が使えないときもある
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(ownerTxt.getBytes(StandardCharsets.UTF_8));
+
+            // Warning: Java17以降からしか使用出来ない
+            // digestの先頭4バイトから符号付きintを生成
+            hash = ByteBuffer.wrap(digest, 0, 4).getInt();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("### SHA-254 algorithm not available" + e);
+        }
 
         // ハッシュ値から色相を決定
         float hue = (hash & 0xFFFFFF) % 360;
 
         // 彩度と明度を制限して、背景色が暗めになるように調整
         float saturation = 0.6f + (Math.abs(hash) % 30) / 100f; // 0.6~0.9
-        float brightness = 0.2f + (Math.abs(hash / 2) % 30) / 100f; // 0.2~0.5
+        float brightness = 0.4f + (Math.abs(hash * 0.6f) % 30) / 100f; // 0.3~0.6
 
         // HSBからColorへ変換
         Color color = Color.getHSBColor(hue / 360f, saturation, brightness);
@@ -126,14 +139,25 @@ public class ScheduleServiceImpl implements ScheduleService {
         // カラーコードへ変換
         String colorCode = String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
 
-        System.out.println(ownerTxt + colorCode);
-
-        // 特別に、特定の人だけ静的に変える(クソ実装)
-        if (colorCode == "#7B0E7D") {
-            colorCode = "#D2691E";
-        } else if (colorCode == "#381436") { // 祝日
-            colorCode = "#A52A2A";
+        // 特別に、特定の人だけ静的に変える(クソ実装。設定とかで変えられうようにしたい)
+        switch (colorCode) {
+            case "#198A24": // 祝日
+                colorCode = "#A52A2A";
+                break;
+            case "#68258F": // n
+                colorCode = "#D2691E";
+                break;
+            case "#22476B": // r
+                colorCode = "#008080";
+                break;
+            case "#738A27": // rn
+                colorCode = "#DB7093";
+                break;
+            default:
+                break;
         }
+
+        System.out.println(ownerTxt + colorCode);
 
         return colorCode;
     }
